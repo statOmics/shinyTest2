935cd918c45626ab784183a78b86885939391c12
lieven clement <lieven.clement@ugent.be>
2018-10-18 19:36:46 +0200

    first commit

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
+source("https://bioconductor.org/biocLite.R")
+biocLite("MSnbase")
